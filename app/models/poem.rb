360153e60aa8298931086982db9df46507cf2d5c
Michael Squitieri <michael.squitieri@gmail.com>
2016-04-23 00:44:34 -0400

    hook up poem => lines association

--- app/models/poem.rb
+++ app/models/poem.rb
@@ -1,2 +1,4 @@
 class Poem < ApplicationRecord
+  has_many :poem_lines
+  has_many :lines, through: :poem_lines
 end
--- app/models/poem_line.rb
+++ app/models/poem_line.rb
@@ -1,2 +1,4 @@
 class PoemLine < ApplicationRecord
+  belongs_to :poem
+  belongs_to :line
 end
