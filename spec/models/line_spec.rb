e18ebc052270d7ce2a0c5b1b999062fe134cf290
Michael Squitieri <michael.squitieri@gmail.com>
2016-04-23 20:42:25 -0400

    add model specs

--- spec/models/line_spec.rb
+++ spec/models/line_spec.rb
@@ -1,16 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe Poem, :type => :model do
+RSpec.describe Line, :type => :model do
   describe 'structure and validations' do
     it 'has its structure' do
       is_expected.to have_db_column(:id)
+      is_expected.to have_db_column(:line_text)
+      is_expected.to have_db_column(:count)
       is_expected.to have_db_column(:updated_at)
       is_expected.to have_db_column(:created_at)
-    end
-
-    it 'has its relationships' do
-      is_expected.to have_many(:poem_lines)
-      is_expected.to have_many(:lines)
     end
   end
 
--- spec/models/poem_line_spec.rb
+++ spec/models/poem_line_spec.rb
@@ -1,16 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe Poem, :type => :model do
+RSpec.describe PoemLine, :type => :model do
   describe 'structure and validations' do
     it 'has its structure' do
       is_expected.to have_db_column(:id)
+      is_expected.to have_db_column(:poem_id)
+      is_expected.to have_db_column(:line_id)
       is_expected.to have_db_column(:updated_at)
       is_expected.to have_db_column(:created_at)
     end
 
     it 'has its relationships' do
-      is_expected.to have_many(:poem_lines)
-      is_expected.to have_many(:lines)
+      is_expected.to belong_to(:poem)
+      is_expected.to belong_to(:line)
     end
   end
 
