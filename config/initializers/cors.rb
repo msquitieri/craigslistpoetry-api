6be934dc1d2e89fd1cb93dad14d88cdd0250962b
Michael Squitieri <michael.squitieri@gmail.com>
2016-06-11 12:07:32 -0400

    allow any domain with get post and options

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,8 +9,8 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
 
-    resource 'api/v1/*',
+    resource '*',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      methods: [:get, :post, :options]
   end
 end
