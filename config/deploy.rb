bbf54bcafccc4edc173680589ca8102a26fd061a
Michael Squitieri <michael.squitieri@gmail.com>
2016-06-18 20:02:05 -0400

    add smtp.yml to the linked files

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,7 +29,7 @@ set :ssh_options, {
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', 'config/smtp.yml')
 
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system')
