04c788d9d2c56b0ea2eda8e65a54ebb5faeb2bee
Michael Squitieri <michael.squitieri@gmail.com>
2017-11-29 21:36:29 -0500

    bump capistrano version

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock '3.8.2'
+lock '3.10.0'
 
 set :application, 'craigslistpoetry-api'
 set :repo_url, 'https://github.com/msquitieri/craigslistpoetry-api.git'
